366d50037512450a7705f3756bcc5761cfb8437c
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-06 20:06:02 +0200

    refactor(Controller): Added login controller to handle form requests

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appAngularjsApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the appAngularjsApp
+ */
+angular.module('appAngularjsApp')
+  .controller('LoginCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
