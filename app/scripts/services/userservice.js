e726135c09331c55090b01825a601f07d02fb7e7
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-06 19:40:51 +0200

    refactor(Service): Added user service

---
+++ app/scripts/services/userservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name appAngularjsApp.userService
+ * @description
+ * # userService
+ * Service in the appAngularjsApp.
+ */
+angular.module('appAngularjsApp')
+  .service('userService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
