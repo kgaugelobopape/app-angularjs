81ae8400d9264edfb8014312c8f2183268d79912
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-07 21:28:53 +0200

    refactor(app): Ejected ui.bootstrap module

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'LocalStorageModule'
+    'LocalStorageModule',
+    'ui.bootstrap'
   ])
   .config(function (localStorageServiceProvider) {
     localStorageServiceProvider
