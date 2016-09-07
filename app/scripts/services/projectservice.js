cef9f2c814896563b77647fd6e0905b9583414dc
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-07 14:01:46 +0200

    refactor(projectservice): Added service to query current projects

---
+++ app/scripts/services/projectservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name appAngularjsApp.projectService
+ * @description
+ * # projectService
+ * Service in the appAngularjsApp.
+ */
+angular.module('appAngularjsApp')
+  .service('projectService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
