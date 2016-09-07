fad72b1240512c53ae74259e0e16d08ddd2504b4
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-07 12:59:38 +0200

    refactor(navbar): Get token from local storage

--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -9,9 +9,5 @@
  */
 angular.module('appAngularjsApp')
   .controller('NavbarCtrl', function ($scope, localStorageService) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.token = localStorageService.get('token');
   });
