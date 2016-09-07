94b8ccf2ec04dac8c04092da516b3aa43c082375
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-07 14:03:13 +0200

    refactor(service): Ejected http service

--- app/scripts/services/projectservice.js
+++ app/scripts/services/projectservice.js
@@ -8,6 +8,6 @@
  * Service in the appAngularjsApp.
  */
 angular.module('appAngularjsApp')
-  .service('projectService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('projectService', function ($http) {
+    
   });
