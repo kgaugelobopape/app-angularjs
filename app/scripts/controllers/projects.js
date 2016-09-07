ff64f04787a293cbd653a35cfb34ad77083cc4b8
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-07 19:13:36 +0200

    refactor(projects): get all projects from projectservice and display on the view

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -8,6 +8,21 @@
  * Controller of the appAngularjsApp
  */
 angular.module('appAngularjsApp')
-  .controller('ProjectsCtrl', function ($scope, localStorageService) {
-    $scope.token = localStorageService.get('token');
+  .controller('ProjectsCtrl', function ($scope, localStorageService, projectService) {
+    
+    if(localStorageService.get('token') !== null){
+        $scope.token = localStorageService.get('token');
+
+        projectService.getProjects()
+          .success(function(data){
+            $scope.projects = data;
+          })
+          .error(function(data){
+            console.log(data);
+          });
+    }
+
+    $scope.viewproject = function(pk){
+
+    };
   });
