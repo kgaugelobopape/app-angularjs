41c7a38afa6a4494ec14a17e551a161607aa90fb
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-07 11:08:15 +0200

    refactor(Route): Added resolve to all routes to check id token is set or not

--- app/scripts/config.router.js
+++ app/scripts/config.router.js
@@ -9,22 +9,50 @@
  * Main module of the application.
  */
 angular
-  .module('appAngularjsApp')
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .otherwise({
-        redirectTo: '/login'
-      });
-  });
+    .module('appAngularjsApp')
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/projects', {
+                templateUrl: 'views/projects.html',
+                controller: 'ProjectsCtrl',
+                resolve: {
+                    factory: ['$location', '$q', 'localStorageService', function ($location, $q, localStorageService) {
+                        var deferred = $q.defer();
+
+                        if (localStorageService.get('token') === null) {
+                            deferred.reject();
+                            $location.path('/login').replace();
+                        } else {
+                            deferred.resolve();
+                        }
+
+                        return deferred.promise();
+                    }]
+                }
+            })
+            .when('/login', {
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl',
+                resolve: {
+                    factory: ['$location', '$q', 'localStorageService', function ($location, $q, localStorageService) {
+                        var deferred = $q.defer();
+
+                        if (localStorageService.get('token') === null) {
+                            deferred.resolve();
+                        } else {
+                            deferred.reject();
+                            $location.path('/projects').replace();
+                        }
+
+                        return deferred.promise;
+                    }]
+                }
+            })
+            .otherwise({
+                redirectTo: '/login'
+            });
+    });
