e90809fb56e387ef9220d2e41a8c6892c0809a09
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-08 18:44:41 +0200

    refactor(controller): Added controller to create new projecy

---
+++ app/scripts/controllers/createproject.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appAngularjsApp.controller:CreateprojectCtrl
+ * @description
+ * # CreateprojectCtrl
+ * Controller of the appAngularjsApp
+ */
+angular.module('appAngularjsApp')
+  .controller('CreateprojectCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
