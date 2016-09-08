fde2a0169193fdc441ed6b1f9086eb350bbee238
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-08 15:13:35 +0200

    refactor(service): Added function service to update project

--- app/scripts/services/projectservice.js
+++ app/scripts/services/projectservice.js
@@ -32,6 +32,15 @@ angular.module('appAngularjsApp')
         });
       },
 
+      putProject: function (pk, data) {
+        return $http.get('http://projectservice.staging.tangentmicroservices.com:80/api/v1/projects/' + pk, data, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + localStorageService.get('token')
+          }
+        });
+      },
+
       getTasks: function () {
         return $http.get('http://projectservice.staging.tangentmicroservices.com/api/v1/tasks/', {
           headers: {
