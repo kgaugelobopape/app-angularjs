15fea987b51f3fe6c2135c3e8def2df4ba9885ea
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-08 18:55:42 +0200

    refactor(controller): Added cancel function to dismiss modal

--- app/scripts/controllers/createproject.js
+++ app/scripts/controllers/createproject.js
@@ -9,9 +9,8 @@
  */
 angular.module('appAngularjsApp')
   .controller('CreateprojectCtrl', function ($scope, $modalInstance) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
   });
