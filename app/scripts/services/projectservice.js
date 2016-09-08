80629b0bc1a9b2317bc95af8dc0d27409f94319f
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-08 15:06:01 +0200

    refactor(service): Added function service to get all tasks from endpoint

--- app/scripts/services/projectservice.js
+++ app/scripts/services/projectservice.js
@@ -9,16 +9,34 @@
  */
 angular.module('appAngularjsApp')
   .service('projectService', function ($http, localStorageService) {
-    
+
     return {
       // ======================= Authentication Session ===========================
       getProjects: function () {
         var token = localStorageService.get('token');
 
-        return $http.get('http://projectservice.staging.tangentmicroservices.com/api/v1/projects/' , {
+        return $http.get('http://projectservice.staging.tangentmicroservices.com/api/v1/projects/', {
           headers: {
             'Content-Type': 'application/json',
-            'Authorization' : 'Token ' + token
+            'Authorization': 'Token ' + localStorageService.get('token')
+          }
+        });
+      },
+
+      getProject: function (pk) {
+        return $http.get('http://projectservice.staging.tangentmicroservices.com:80/api/v1/projects/' + pk, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + localStorageService.get('token')
+          }
+        });
+      },
+
+      getTasks: function () {
+        return $http.get('http://projectservice.staging.tangentmicroservices.com/api/v1/tasks/', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + localStorageService.get('token')
           }
         });
       }
