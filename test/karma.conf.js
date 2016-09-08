05387d808eeece31565de3f69b508e953b097ed5
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-08 15:07:27 +0200

    Added angular-mocks

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-local-storage/dist/angular-local-storage.js',
-      'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
