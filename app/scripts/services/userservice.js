1e814aa6fcaf0b1690bd2b13b711f5f095994bbb
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-08 22:31:17 +0200

    refactor(service): Added function service to retrive user details

--- app/scripts/services/userservice.js
+++ app/scripts/services/userservice.js
@@ -8,7 +8,7 @@
  * Service in the appAngularjsApp.
  */
 angular.module('appAngularjsApp')
-  .service('userService', function ($http) {
+  .service('userService', function ($http, localStorageService) {
 
     return {
       // ======================= Authentication Session ===========================
@@ -16,6 +16,15 @@ angular.module('appAngularjsApp')
         return $http.post('http://userservice.staging.tangentmicroservices.com/api-token-auth/', data , {
           headers: {
             'Content-Type': 'application/json'
+          }
+        });
+      },
+
+      getProfile: function () {
+        return $http.get('http://userservice.staging.tangentmicroservices.com:80/api/v1/users/me/' , {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Token ' + localStorageService.get('token')
           }
         });
       }
