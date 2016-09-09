9eec9ea0f4fb96e04d971564ed6479118c8327bd
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-09 17:43:31 +0200

    refactor(service): Unit tested service to login and get user profile

--- test/spec/services/userservice.js
+++ test/spec/services/userservice.js
@@ -6,13 +6,44 @@ describe('Service: userService', function () {
   beforeEach(module('appAngularjsApp'));
 
   // instantiate service
-  var userService;
-  beforeEach(inject(function (_userService_) {
+  var userService,
+    $httpBackend,
+    localStorageService;
+
+  beforeEach(inject(function (_userService_, _$httpBackend_, _localStorageService_) {
     userService = _userService_;
+    $httpBackend = _$httpBackend_;
+    localStorageService = _localStorageService_;
+
+    spyOn(localStorageService, 'get').and.returnValue({ token: 'mockToken' });
   }));
 
-  it('should do something', function () {
-    expect(!!userService).toBe(true);
-  });
+  it('should authenticate user with username and passwrod', inject(function (userService) {
+
+    $httpBackend.expectPOST('http://userservice.staging.tangentmicroservices.com/api-token-auth/')
+      .respond('response');
+
+    userService.login()
+      .success(function (data, status) {
+        expect(status).toEqual(200);
+        expect(data).toEqual('response');
+      });
+
+    $httpBackend.flush();
+
+  }));
+
+  it('should get the user profile', inject(function (userService) {
+    $httpBackend.expectGET('http://userservice.staging.tangentmicroservices.com:80/api/v1/users/me/')
+      .respond('response');
+
+    userService.getProfile().success(function (data, status) {
+      expect(status).toEqual(200);
+      expect(data).toEqual('response');
+    });
+
+    $httpBackend.flush();
+
+  }));
 
 });
