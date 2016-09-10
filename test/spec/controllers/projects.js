fe76bee74a1c25bfcd76075bfc12fd47dd903120
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-10 17:03:52 +0200

    Testing project controller to access application

--- test/spec/controllers/projects.js
+++ test/spec/controllers/projects.js
@@ -6,17 +6,130 @@ describe('Controller: ProjectsCtrl', function () {
   beforeEach(module('appAngularjsApp'));
 
   var ProjectsCtrl,
-    scope;
+    scope,
+    localStorageService,
+    projectService,
+    modal,
+    $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, _localStorageService_, _$modal_ ,_projectService_) {
     scope = $rootScope.$new();
+    localStorageService = _localStorageService_;
+    projectService = _projectService_;
+    modal = _$modal_;
+
     ProjectsCtrl = $controller('ProjectsCtrl', {
-      $scope: scope
+      $scope: scope,
+      $modal: modal
     });
+
+
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should set up correctly', function () {
+
+    expect(scope.ploading).toEqual(true);
+    expect(scope.tloading).toEqual(true);
+    spyOn(localStorageService, 'get').and.returnValue({ token: 'mockToken' });
+
+  });
+
+  it('should get a list of projects', function () {
+
+    projectService.getProjects()
+      .success(function (data, status) {
+        expect(status).toEqual(200);
+        expect(data).toEqual('response');
+
+        expect(scope.projects).toEqual(data);
+        expect(scope.ploading).toEqual(false);
+      });
+  });
+
+  it('should get a list of tasks', function () {
+
+    projectService.getTasks()
+      .success(function (data, status) {
+        expect(status).toEqual(200);
+        expect(data).toEqual('response');
+        expect(scope.projects).toEqual(data);
+        expect(scope.tloading).toEqual(false);
+      });
+  });
+
+  it('should open the edit project modal', function () {
+    
+    var actualOptions;
+    var fakeModal = {
+      result: {
+        then: function(confirmCallback) {
+          this.confirmCallBack = confirmCallback;
+        }
+      }
+    };
+
+    var modalOptions = {
+      templateUrl: 'views/modal/create.html',
+      controller: 'CreateprojectCtrl',
+      resolve: {
+        pk: jasmine.any(Function)
+      }
+    };
+
+    spyOn(modal, 'open').and.callFake(function(options) {
+      actualOptions = options;
+      return fakeModal;
+    });
+  });
+
+  it('should open the create project modal', function () {
+
+    var actualOptions;
+    var fakeModal = {
+      result: {
+        then: function(confirmCallback) {
+          this.confirmCallBack = confirmCallback;
+        }
+      }
+    };
+
+    var modalOptions = {
+      templateUrl: 'views/modal/create.html',
+      controller: 'CreateprojectCtrl',
+      resolve: {
+        pk: jasmine.any(Function)
+      }
+    };
+
+    spyOn(modal, 'open').and.callFake(function(options) {
+      actualOptions = options;
+      return fakeModal;
+    });
+  });
+
+  it('should open the delete project modal', function () {
+
+    var actualOptions;
+    var fakeModal = {
+      result: {
+        then: function(confirmCallback) {
+          this.confirmCallBack = confirmCallback;
+        }
+      }
+    };
+
+    var modalOptions = {
+      templateUrl: 'views/modal/delete.html',
+      controller: 'DeleteprojectCtrl',
+      resolve: {
+        pk: jasmine.any(Function)
+      }
+    };
+
+    spyOn(modal, 'open').and.callFake(function(options) {
+      actualOptions = options;
+      return fakeModal;
+    });
   });
 });
