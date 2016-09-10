31c7f8c8c54e670b46516f4faa7f6070b6d08b6c
Kgaugelo Bopape <kgaugelo20@gmail.com>
2016-09-10 13:37:35 +0200

    Testing test contoller to create new project

---
+++ test/spec/controllers/createproject.js
@@ -0,0 +1,73 @@
+'use strict';
+
+describe('Controller: CreateprojectCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('appAngularjsApp'));
+
+  var CreateprojectCtrl,
+    scope,
+    modalInstance,
+    $httpBackend,
+    projectService,
+    timeout,
+    route;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope, _$httpBackend_, _projectService_, _$timeout_, _$route_) {
+    scope = $rootScope.$new();
+    // modalInstance = _$modalInstance_;
+    projectService = _projectService_;
+    timeout = _$timeout_;
+    route = _$route_;
+    $httpBackend = _$httpBackend_;
+
+    CreateprojectCtrl = $controller('CreateprojectCtrl', {
+      $scope: scope,
+      $modalInstance: modalInstance
+    });
+
+    // Create a mock object using spies
+    modalInstance = {
+      close: jasmine.createSpy('modalInstance.close'),
+      dismiss: jasmine.createSpy('modalInstance.dismiss'),
+      result: {
+        then: jasmine.createSpy('modalInstance.result.then')
+      }
+    };
+  }));
+
+  it('should be set up correctly', function () {
+    expect(scope.loading).toBe(false);
+    expect(scope.creating).toBe(false);
+    expect(scope.error).toEqual('');
+    expect(scope.success).toEqual('');
+  });
+
+  it('should be configured to create a project', function () {
+
+    $httpBackend.expectPOST('http://projectservice.staging.tangentmicroservices.com:80/api/v1/projects/', { title: 'mockTitle', description: 'Unit testing', start_date: '2015-09-09', end_date: '2016-09-09', is_billable: 'yes', is_active: 'yes' })
+      .respond('response');
+
+    scope.submit();
+    expect(scope.creating).toEqual(true);
+    projectService.postProject()
+      .success(function (data, status) {
+
+        expect(status).toEqual(201);
+        expect(data).toEqual('response');
+
+        expect(scope.creating).toEqual(false);
+        expect(scope.success).toEqual(true);
+        expect(scope.project).toBeNull();
+      })
+      .error(function(){
+        expect(scope.creating).toBe(false);
+        expect(status).toEqual(201);
+        expect(data).toEqual('response');
+        expect(scope.error).toEqual('response');
+      });
+  });
+
+
+});
